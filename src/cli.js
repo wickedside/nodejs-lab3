786297d58c14051085470f99b50b70fcdc07ad58
wickedside <w1ck3ds1d3@gmail.com>
2024-05-09 16:21:56 +0300

    done cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,31 @@
 import minimist from 'minimist';
+import { uncollapse } from './uncollapse.js';
+import { matrixAddition } from './matrixAddition.js';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { Transform } from 'stream';
 
 const args = minimist(process.argv.slice(2));
 
 const handleInput = (input, output, task) => {
-  // TODO: Реализовать логику обработки входных данных
-};
+    const inputSrc = input ? createReadStream(input) : process.stdin;
+    const outputSrc = output ? createWriteStream(output) : process.stdout;
+  
+    const transformStream = new Transform({
+      transform(chunk, encoding, callback) {
+        let data;
+        if (task === 'uncollapse') {
+          data = uncollapse(chunk.toString());
+        } else if (task === 'matrixAddition') {
+          data = JSON.stringify(matrixAddition(JSON.parse(chunk.toString())));
+        }
+        this.push(data);
+        callback();
+      }
+    });
+  
+    return pipeline(inputSrc, transformStream, outputSrc);
+  };
 
 const { input, output, task } = args;
 
