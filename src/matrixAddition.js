3365df00cf4e641419d663e44055f49a9dfa077b
wickedside <w1ck3ds1d3@gmail.com>
2024-05-09 16:09:06 +0300

    uncollapse and matrixAddition done

--- src/matrixAddition.js
+++ src/matrixAddition.js
@@ -1,3 +1,3 @@
 export const matrixAddition = (matrix1, matrix2) => {
-    // TODO: Реализовать функцию matrixAddition
+    return matrix1.map((row, i) => row.map((cell, j) => cell + matrix2[i][j]));
   };  
--- src/uncollapse.js
+++ src/uncollapse.js
@@ -1,3 +1,5 @@
 export const uncollapse = (digits) => {
-    // TODO: Реализовать функцию uncollapse
-  };  
+    const words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    const regex = new RegExp(words.join('|'), 'gi');
+    return digits.match(regex).join(' ');
+  };
