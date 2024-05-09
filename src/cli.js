a1729727a28ec1cd3b25d38e536f9fb1b9df75e3
wickedside <w1ck3ds1d3@gmail.com>
2024-05-09 16:45:41 +0300

    fixed matrixAddition function and added test files

--- src/cli.js
+++ src/cli.js
@@ -5,27 +5,46 @@ import { createReadStream, createWriteStream } from 'fs';
 import { pipeline } from 'stream/promises';
 import { Transform } from 'stream';
 
-const args = minimist(process.argv.slice(2));
+const args = minimist(process.argv.slice(2), {
+    alias: {
+      t: 'task',
+      i: 'input',
+      o: 'output'
+    }
+  });
 
-const handleInput = (input, output, task) => {
-    const inputSrc = input ? createReadStream(input) : process.stdin;
+  const handleInput = (input, output, task) => {
+    const inputSrc = input ? createReadStream(input, 'utf8') : process.stdin;
     const outputSrc = output ? createWriteStream(output) : process.stdout;
-  
+
     const transformStream = new Transform({
-      transform(chunk, encoding, callback) {
-        let data;
-        if (task === 'uncollapse') {
-          data = uncollapse(chunk.toString());
-        } else if (task === 'matrixAddition') {
-          data = JSON.stringify(matrixAddition(JSON.parse(chunk.toString())));
-        }
-        this.push(data);
-        callback();
-      }
+        transform(chunk, encoding, callback) {
+            let data;
+            if (task === 'uncollapse') {
+                data = uncollapse(chunk.toString());
+            } else if (task === 'matrixAddition') {
+                try {
+                    const matrices = JSON.parse(chunk.toString());
+                    if (matrices.length !== 2) {
+                        throw new Error('Должно быть 2 матрицы');
+                    }
+                    data = JSON.stringify(matrixAddition(matrices[0], matrices[1]));
+                } catch (err) {
+                    return callback(err);
+                }
+            }
+            this.push(data);
+            callback();
+        },
+        readableObjectMode: true,
+        writableObjectMode: true
     });
-  
-    return pipeline(inputSrc, transformStream, outputSrc);
-  };
+
+    return pipeline(inputSrc, transformStream, outputSrc)
+        .catch(err => {
+            console.error('Pipeline failed:', err);
+        });
+};
 
 const { input, output, task } = args;
 
--- src/matrixAddition.js
+++ src/matrixAddition.js
@@ -1,3 +1,8 @@
 export const matrixAddition = (matrix1, matrix2) => {
-    return matrix1.map((row, i) => row.map((cell, j) => cell + matrix2[i][j]));
-  };  
+    if (!matrix1 || !matrix2 || matrix1.length !== matrix2.length || matrix1.some((row, i) => row.length !== matrix2[i].length)) {
+        throw new Error("Матрицы должны быть одинаковые и не пустые");
+    }
+    return matrix1.map((row, i) => 
+        row.map((cell, j) => cell + matrix2[i][j])
+    );
+};
